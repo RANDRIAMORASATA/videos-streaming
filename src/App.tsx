4a4a6d472b33392c9ed1934fe7818ade64b4c356
Angella R <randriamorasataangello@gmail.com>
2024-11-14 16:12:28 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Account from './pages/Account/Account';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import MediaReader from './components/MediaReader/MediaReader';
+import Notification from './components/Notification/Notification';
+import { Video } from './models/Video';
 
 function App() {
+  const [videos, setVideos] = useState<Video[]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home handleChange={setVideos} />} /> {/* Pass the setter here */}
+        <Route path="/account" element={<Account />} />
+        <Route path="/reader/:slug" element={<MediaReader />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
+      <Notification />
+    </BrowserRouter>
   );
 }
 
